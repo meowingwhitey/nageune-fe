3e9180f393977ac9343110c1e2221b843d196d00
ds10x2 <ds10x2@gmail.com>
2024-05-19 23:17:19 +0900

    ADD: Footer 작성

---
+++ src/components/includes/CopyrightComponent.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <img width="250px" src="@/assets/img_opentype04.jpg" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/includes/FooterComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CopyrightComponent from "@/components/includes/CopyrightComponent.vue";
+</script>
+
+<template>
+  <div id="footer">
+    <v-container>
+      <div class="mt-5 d-flex justify-center">
+        <CopyrightComponent />
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+#footer {
+  background-color: rgb(158, 158, 158);
+  height: 250px;
+}
+</style>
