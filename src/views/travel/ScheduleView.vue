8f0a06d33deff26d741702d67a21d48f36a739da
meowingwhitey <kyp0416@gmail.com>
2024-05-23 09:45:58 +0900

    FIX: 출발일 변경X시 하루가 줄어드는 버그 수정, MOD: 출발지 페이지 삭제

--- src/views/travel/ScheduleView.vue
+++ src/views/travel/ScheduleView.vue
@@ -16,7 +16,8 @@ const allowedEndDate = ref({
   min: "0000-01-01",
   max: "9999-12-31",
 });
-const startDate = ref(new Date());
+// 00:00:00부터 계산하기 위해 아래처럼 출발일 설정
+const startDate = ref(new Date(format(new Date(), "MM/dd/yyyy")));
 const endDate = ref(addDays(startDate.value, 6));
 const travelStore = useTravelStore();
 
@@ -43,7 +44,7 @@ onMounted(() => {
 });
 
 const onPreviousClick = () => {
-  router.push({ name: "travel-departure" });
+  router.push({ name: "travel-create" });
 };
 
 const onNextClick = () => {
@@ -118,7 +119,7 @@ const onNextClick = () => {
             color="#90A4AE"
             append-icon="mdi-arrow-u-left-top"
           >
-            우리집이 어디더라?
+            여행 닉변권 쓰기
           </v-btn>
           <v-btn
             @click="onNextClick"
